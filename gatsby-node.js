56b626b8754bf2bc5427401696b339628c7b5fbb
Ashton <ashchristie@gmail.com>
2018-08-29 19:55:28 -0700

    11

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,54 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+// See: https://www.gatsbyjs.org/docs/node-apis/
+const path = require('path')
+const { createFilePath } = require('gatsby-source-filesystem')
 
- // You can delete this file if you're not using it
+// onCreateNode runs on every page, this will check if page is MD file, create slug for each page
+exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
+  const { createNodeField } = boundActionCreators
+  if (node.internal.type === 'MarkdownRemark') {
+    const slug = createFilePath({
+      node,
+      getNode,
+      basePath: 'posts',
+    })
+    createNodeField({
+      node,
+      name: 'slug',
+      value: `/posts${slug}`,
+    })
+  }
+}
+
+// create blog post pages
+exports.createPages = ({ graphql, boundActionCreators }) => {
+  const { createPage } = boundActionCreators
+  // create promise to get slugs
+  return new Promise((resolve, reject) => {
+    graphql(`
+      {
+        allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `).then(result => {
+      // create new page for each slug found
+      result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+        createPage({
+          path: node.fields.slug,
+          component: path.resolve('./src/pages/about.js'),
+          context: {
+            // variable you assign to graphql to pass
+            slug: node.fields.slug,
+          },
+        })
+      })
+    })
+    resolve()
+  })
+}
